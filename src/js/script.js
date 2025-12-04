7b714d81401719fd113ef84716550498cee9bbc3
wissal-lang <wissalkhouja990@gmail.com>
2025-12-04 12:02:44 +0100

    Implement daily spending loop for user balance

--- src/js/script.js
+++ src/js/script.js
@@ -17,4 +17,11 @@ let daysSurvived = 0;
 console.log("User Pets:", userPets);
 console.log("User Balance:", userBalance);
 console.log("Every Day Spending Per Pet:", everyDaySpendingPerPet);
+while (userBalance > 0) { 
+ let spending = EVERY_DAY_SPENDING + everyDaySpendingPerPet * userPets.length 
+ userBalance -= spending 
+ daysSurvived++ 
+} 
+ 
+console.log("User have sufficient money for " + daysSurvived + " days") 
 
