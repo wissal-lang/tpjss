c94b3a69b45bd6b173710322e13adeb93e5a18f7
wissal-lang <wissalkhouja990@gmail.com>
2025-12-04 12:01:04 +0100

    Add additional console logs for user details

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,16 @@ let userAge = 21
  
 console.log("User Name", userName) 
 console.log("User Age", userAge)
+console.log("User Name:", userName);
+console.log("User Age:", userAge);
+
+let userPets = ["Cat", "Dog"];
+let userBalance = 1200;
+const EVERY_DAY_SPENDING = 15.3;
+let everyDaySpendingPerPet = 6;
+let daysSurvived = 0;
+
+console.log("User Pets:", userPets);
+console.log("User Balance:", userBalance);
+console.log("Every Day Spending Per Pet:", everyDaySpendingPerPet);
+
