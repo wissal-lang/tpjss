3b803b6923110b78bfc52f3e53ed164244d50ac8
wissal-lang <wissalkhouja990@gmail.com>
2025-12-04 11:57:52 +0100

    Add userName and userAge variables with logging

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
-
+let userName = "Andy" 
+let userAge = 45 
+ 
+//console.log("Text", variable) allows you to write to the console 
+ 
+console.log("User Name", userName) 
+console.log("User Age", userAge)
